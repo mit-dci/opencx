8faa04197656342a312e5f17a6e435f69d30b9bb
Rjected <kidscline01@gmail.com>
2019-02-02 23:16:05 -0500

    Adding some helper stuff for orders

--- match/order.go
+++ match/order.go
@@ -1,5 +1,7 @@
 package match
 
+import "fmt"
+
 // Pair is a struct that represents a trading pair
 type Pair struct {
 	AssetWant byte `json:"assetWant"`
@@ -8,22 +10,40 @@ type Pair struct {
 }
 
 // Order is a struct that represents a stored side of a trade
-type Order struct {
-	Client      string  `json:"username"`
-	Side        string  `json:"side"`
-	TradingPair Pair    `json:"pair"`
+type Order interface {
+	Type() string
+	Price() (float64, error)
+}
+
+// LimitOrder represents a limit order, implementing the order interface
+type LimitOrder struct {
+	Client      string `json:"username"`
+	Side        string `json:"side"`
+	TradingPair Pair   `json:"pair"`
 	// amount of assetHave the user would like to trade
-	AmountHave  int64   `json:"amount"`
+	AmountHave uint64 `json:"amount"`
 	// amount of assetWant the user wants for their assetHave
-	AmountWant  int64    `json:"price"`
+	AmountWant uint64 `json:"price"`
 }
 
-func(o *Order) isBuySide() bool {
-	return o.Side == "buy"
+// IsBuySide returns true if the limit order is buying
+func (l *LimitOrder) IsBuySide() bool {
+	return l.Side == "buy"
 }
 
-func(o *Order) isSellSide() bool {
-	return o.Side == "sell"
+// IsSellSide returns true if the limit order is selling
+func (l *LimitOrder) IsSellSide() bool {
+	return l.Side == "sell"
+}
+
+// Price gets a float price for the order. This determines how it will get matched. The exchange should figure out if it can take some of the
+// pennies off the dollar for things that request a certain amount but the amount they get (according to price and what the other side would be willing
+// to give) is less than they officially requested. But tough luck to them we're taking fees anyways
+func (l *LimitOrder) Price() (float64, error) {
+	if l.AmountWant == 0 {
+		return 0, fmt.Errorf("The amount requested in the order is 0, so no price can be calculated. Consider it a donation")
+	}
+	return float64(l.AmountHave / l.AmountWant), nil
 }
 
 // generateUniquePairs generates unique asset pairs based on the assets available
@@ -34,7 +54,7 @@ func generateUniquePairs(assetList []byte) []Pair {
 	var pairList = make([]Pair, numPairIndeces)
 	pairListIndex := 0
 	for i, elem := range assetList {
-		for lower := i+1; lower < assetListLen; lower++ {
+		for lower := i + 1; lower < assetListLen; lower++ {
 			pairList[pairListIndex].AssetWant = elem
 			pairList[pairListIndex].AssetHave = assetList[lower]
 			pairListIndex++
@@ -50,6 +70,6 @@ func GenerateAssetPairs() []Pair {
 }
 
 // String is the tostring function for a pair
-func(p Pair) String() string {
+func (p Pair) String() string {
 	return ByteToAssetString(p.AssetWant) + "/" + ByteToAssetString(p.AssetHave)
 }
