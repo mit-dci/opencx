ac803cca3f94b5f17d6e329cdff56e96454cbe67
Rjected <kidscline01@gmail.com>
2019-04-12 14:26:29 -0400

    Fixed cxbenchmark tests

--- cxbenchmark/matchingbenchmark.go
+++ cxbenchmark/matchingbenchmark.go
@@ -1,6 +1,7 @@
 package cxbenchmark
 
 import (
+	"github.com/mit-dci/lit/crypto/koblitz"
 	"github.com/mit-dci/opencx/cmd/benchclient"
 	"github.com/mit-dci/opencx/cxrpc"
 	"github.com/mit-dci/opencx/logging"
@@ -9,15 +10,15 @@ import (
 // TODO: make this work with public keys. Currently doesn't work.
 
 // PlaceAndFill places a whole bunch of orders (in goroutines of course) and fills a whole bunch of orders.
-func PlaceAndFill(client *benchclient.BenchClient, user1 string, user2 string, pair string, howMany int) {
+func PlaceAndFill(client *benchclient.BenchClient, user1Pub *koblitz.PublicKey, user2Pub *koblitz.PublicKey, pair string, howMany int) {
 	for i := 0; i < howMany; i++ {
 		// This shouldnt make any change in balance but each account should have at least 2000 satoshis (or the smallest unit in whatever chain)
 		bufErrChan := make(chan error, 4)
 		orderChan := make(chan *cxrpc.SubmitOrderReply)
-		go client.OrderAsync(user1, "buy", pair, 1000, 1.0, orderChan, bufErrChan)
-		go client.OrderAsync(user2, "sell", pair, 1000, 1.0, orderChan, bufErrChan)
-		go client.OrderAsync(user1, "sell", pair, 2000, 2.0, orderChan, bufErrChan)
-		go client.OrderAsync(user2, "buy", pair, 1000, 2.0, orderChan, bufErrChan)
+		go client.OrderAsync(user1Pub, "buy", pair, 1000, 1.0, orderChan, bufErrChan)
+		go client.OrderAsync(user2Pub, "sell", pair, 1000, 1.0, orderChan, bufErrChan)
+		go client.OrderAsync(user1Pub, "sell", pair, 2000, 2.0, orderChan, bufErrChan)
+		go client.OrderAsync(user2Pub, "buy", pair, 1000, 2.0, orderChan, bufErrChan)
 
 		for i := 0; i < cap(bufErrChan); i++ {
 			if err := <-bufErrChan; err != nil {
@@ -29,11 +30,11 @@ func PlaceAndFill(client *benchclient.BenchClient, user1 string, user2 string, p
 }
 
 // PlaceManyBuy places many orders at once
-func PlaceManyBuy(client *benchclient.BenchClient, user1 string, user2 string, pair string, howMany int) {
+func PlaceManyBuy(client *benchclient.BenchClient, user1Pub *koblitz.PublicKey, user2Pub *koblitz.PublicKey, pair string, howMany int) {
 	bufErrChan := make(chan error, howMany)
 	orderChan := make(chan *cxrpc.SubmitOrderReply)
 	for i := 0; i < howMany; i++ {
-		go client.OrderAsync(user1, "buy", pair, 1000, 1.0, orderChan, bufErrChan)
+		go client.OrderAsync(user1Pub, "buy", pair, 1000, 1.0, orderChan, bufErrChan)
 	}
 	for i := 0; i < cap(bufErrChan); i++ {
 		if err := <-bufErrChan; err != nil {
@@ -44,11 +45,11 @@ func PlaceManyBuy(client *benchclient.BenchClient, user1 string, user2 string, p
 }
 
 // PlaceManySell places many orders at once
-func PlaceManySell(client *benchclient.BenchClient, user1 string, user2 string, pair string, howMany int) {
+func PlaceManySell(client *benchclient.BenchClient, user1Pub *koblitz.PublicKey, user2Pub *koblitz.PublicKey, pair string, howMany int) {
 	bufErrChan := make(chan error, howMany)
 	orderChan := make(chan *cxrpc.SubmitOrderReply)
 	for i := 0; i < howMany; i++ {
-		go client.OrderAsync(user1, "sell", pair, 1000, 1.0, orderChan, bufErrChan)
+		go client.OrderAsync(user1Pub, "sell", pair, 1000, 1.0, orderChan, bufErrChan)
 	}
 	for i := 0; i < cap(bufErrChan); i++ {
 		if err := <-bufErrChan; err != nil {
@@ -60,18 +61,51 @@ func PlaceManySell(client *benchclient.BenchClient, user1 string, user2 string, 
 
 // PlaceBuySellX runs functions with predone parameters, you only tell it how many times it should run and what client to use
 func PlaceBuySellX(client *benchclient.BenchClient, varRuns int) {
-	PlaceManyBuy(client, "tester", "othertester", "btc/ltc", varRuns)
-	PlaceManyBuy(client, "tester", "othertester", "btc/vtc", varRuns)
-	PlaceManyBuy(client, "tester", "othertester", "vtc/ltc", varRuns)
-	PlaceManySell(client, "tester", "othertester", "btc/ltc", varRuns)
-	PlaceManySell(client, "tester", "othertester", "btc/vtc", varRuns)
-	PlaceManySell(client, "tester", "othertester", "vtc/ltc", varRuns)
+	var err error
+
+	var testerPrivKey *koblitz.PrivateKey
+	if testerPrivKey, err = koblitz.NewPrivateKey(koblitz.S256()); err != nil {
+		logging.Errorf("Error creating new private key for tester: %s", err)
+		return
+	}
+
+	var otherTesterPrivKey *koblitz.PrivateKey
+	if otherTesterPrivKey, err = koblitz.NewPrivateKey(koblitz.S256()); err != nil {
+		logging.Errorf("Error creating new private key for otherTester: %s", err)
+		return
+	}
+
+	testerPubKey := testerPrivKey.PubKey()
+	otherTesterPubKey := otherTesterPrivKey.PubKey()
+
+	PlaceManyBuy(client, testerPubKey, otherTesterPubKey, "btc/ltc", varRuns)
+	PlaceManyBuy(client, testerPubKey, otherTesterPubKey, "btc/vtc", varRuns)
+	PlaceManyBuy(client, testerPubKey, otherTesterPubKey, "vtc/ltc", varRuns)
+	PlaceManySell(client, testerPubKey, otherTesterPubKey, "btc/ltc", varRuns)
+	PlaceManySell(client, testerPubKey, otherTesterPubKey, "btc/vtc", varRuns)
+	PlaceManySell(client, testerPubKey, otherTesterPubKey, "vtc/ltc", varRuns)
 	return
 }
 
 // PlaceFillX runs functions with predone parameters, you only tell it how many times it should run and what client to use
 func PlaceFillX(client *benchclient.BenchClient, varRuns int) {
-	PlaceAndFill(client, "tester", "othertester", "btc/ltc", varRuns)
-	PlaceAndFill(client, "tester", "othertester", "btc/vtc", varRuns)
-	PlaceAndFill(client, "tester", "othertester", "vtc/ltc", varRuns)
+	var err error
+
+	var testerPrivKey *koblitz.PrivateKey
+	if testerPrivKey, err = koblitz.NewPrivateKey(koblitz.S256()); err != nil {
+		logging.Errorf("Error creating new private key for tester: %s", err)
+		return
+	}
+
+	var otherTesterPrivKey *koblitz.PrivateKey
+	if otherTesterPrivKey, err = koblitz.NewPrivateKey(koblitz.S256()); err != nil {
+		logging.Errorf("Error creating new private key for otherTester: %s", err)
+		return
+	}
+
+	testerPubKey := testerPrivKey.PubKey()
+	otherTesterPubKey := otherTesterPrivKey.PubKey()
+	PlaceAndFill(client, testerPubKey, otherTesterPubKey, "btc/ltc", varRuns)
+	PlaceAndFill(client, testerPubKey, otherTesterPubKey, "btc/vtc", varRuns)
+	PlaceAndFill(client, testerPubKey, otherTesterPubKey, "vtc/ltc", varRuns)
 }
