854444f4f6a81e2ffb1808203bea49a4e27e7349
Rjected <kidscline01@gmail.com>
2019-04-25 16:57:54 -0400

    Removed puzzle, adding solvers

---
+++ crypto/timelocksolvers/blocksolver.go
@@ -0,0 +1,86 @@
+package crypto
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"fmt"
+
+	"github.com/dgryski/go-rc5"
+	"github.com/dgryski/go-rc6"
+	"github.com/mit-dci/opencx/crypto"
+)
+
+// SolvePuzzleRC5 solves the timelock puzzle and decrypts the ciphertext using RC5
+func SolvePuzzleRC5(ciphertext []byte, puzzle crypto.Puzzle) (message []byte, err error) {
+	if puzzle == nil {
+		err = fmt.Errorf("Puzzle cannot be nil, what are you solving")
+		return
+	}
+
+	var key []byte
+	if key, err = puzzle.Solve(); err != nil {
+		err = fmt.Errorf("Error solving auction puzzle: %s", err)
+		return
+	}
+
+	var RC5Cipher cipher.Block
+	if RC5Cipher, err = rc5.New(key); err != nil {
+		err = fmt.Errorf("Could not create new rc5 cipher for puzzle: %s", err)
+		return
+	}
+
+	// Decrypt doesn't return anything
+	RC5Cipher.Decrypt(message, ciphertext)
+
+	return
+}
+
+// SolvePuzzleRC6 solves the timelock puzzle and decrypts the ciphertext using RC6
+func SolvePuzzleRC6(ciphertext []byte, puzzle crypto.Puzzle) (message []byte, err error) {
+	if puzzle == nil {
+		err = fmt.Errorf("Puzzle cannot be nil, what are you solving")
+		return
+	}
+
+	var key []byte
+	if key, err = puzzle.Solve(); err != nil {
+		err = fmt.Errorf("Error solving auction puzzle: %s", err)
+		return
+	}
+
+	var RC6Cipher cipher.Block
+	if RC6Cipher, err = rc6.New(key); err != nil {
+		err = fmt.Errorf("Could not create new rc6 cipher for puzzle: %s", err)
+		return
+	}
+
+	// Decrypt doesn't return anything
+	RC6Cipher.Decrypt(message, ciphertext)
+
+	return
+}
+
+// SolvePuzzleAES solves the timelock puzzle and decrypts the ciphertext using AES
+func SolvePuzzleAES(ciphertext []byte, puzzle crypto.Puzzle) (message []byte, err error) {
+	if puzzle == nil {
+		err = fmt.Errorf("Puzzle cannot be nil, what are you solving")
+		return
+	}
+
+	var key []byte
+	if key, err = puzzle.Solve(); err != nil {
+		err = fmt.Errorf("Error solving auction puzzle: %s", err)
+		return
+	}
+
+	var AESCipher cipher.Block
+	if AESCipher, err = aes.NewCipher(key); err != nil {
+		err = fmt.Errorf("Could not create new aes cipher for puzzle: %s", err)
+		return
+	}
+
+	// Decrypt doesn't return anything
+	AESCipher.Decrypt(message, ciphertext)
+
+	return
+}
--- match/auctionpuzzle.go
+++
@@ -1,8 +0,0 @@
-package match
-
-// AuctionPuzzle holds the encrypted message and the puzzle. The message when decrypted with the correct
-// puzzle answer should be serializable to an auction order.
-type AuctionPuzzle struct {
-	puzzle  []byte
-	message []byte
-}
