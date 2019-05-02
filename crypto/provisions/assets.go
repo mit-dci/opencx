92a3a44d1412ed0629c66185440500a081c75af6
Rjected <kidscline01@gmail.com>
2019-05-02 12:35:17 -0400

    Defining proof of assets

--- crypto/provisions/assets.go
+++ crypto/provisions/assets.go
@@ -17,6 +17,8 @@ type BalProofMachine struct {
 	u3 *big.Int
 	u4 *big.Int
 	ci *big.Int
+	ti *big.Int
+	xi *big.Int
 }
 
 // NewBalProofMachine creates a new balance proof machine
@@ -46,8 +48,22 @@ func NewBalProofMachine(curve elliptic.Curve) (machine *BalProofMachine, err err
 }
 
 // SetChallenge sets the challenge so we can generate responses
-func (machine *BalProofMachine) SetChallenge(ci *big.Int) {
+func (machine *BalProofMachine) SetChallenge(ci *big.Int) (err error) {
+	if ci == nil {
+		err = fmt.Errorf("Challenge cannot be nil")
+		return
+	}
 	machine.ci = ci
+	return
+}
+
+// SetPrivKey sets the private key for this iteration
+func (machine *BalProofMachine) SetPrivKey(privkey *ecdsa.PrivateKey) (err error) {
+	if privkey == nil {
+		err = fmt.Errorf("Private key cannot be nil")
+		return
+	}
+	machine.xi = privkey.D
 	return
 }
 
@@ -70,6 +86,7 @@ func (machine *BalProofMachine) SResponse(si bool) (rs *big.Int, err error) {
 // AssetsProofMachine is the state machine that is used to create a privacy preserving proof of assets
 type AssetsProofMachine struct {
 	curve elliptic.Curve
-	// PrivKeySet is the set of all x_i
-	PrivKeySet []*ecdsa.PrivateKey
+	// This is the anonymity set PK in the paper, !(privkey == nil) => s_i = 0
+	PubKeyAnonSet    map[*ecdsa.PublicKey]*ecdsa.PrivateKey
+	BalanceRetreiver func(pubkey *ecdsa.PublicKey) (balance uint64, err error)
 }
