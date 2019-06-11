572d372192450d12b1b6fee5d3d3b0eb31a3313b
Rjected <kidscline01@gmail.com>
2019-06-10 23:14:26 -0400

    Starting with batching, removed printf
    Need to keep track of things for batch

--- cxauctionserver/handlers.go
+++ cxauctionserver/handlers.go
@@ -27,3 +27,28 @@ func (s *OpencxAuctionServer) AuctionOrderHandler(orderResultChannel chan *match
 
 	}
 }
+
+// HandleForAuction tries to receive all solved auction orders for a specific auction, batches them up, and then places the batch.
+// chanSize is the number of orders that should be in this auction.
+func (s *OpencxAuctionServer) HandleForAuction(auctionResultChannel chan *match.OrderPuzzleResult, chanSize uint64) {
+	// We can reuse these, do not put them in the infinite loop
+	var receivedOrder *match.OrderPuzzleResult
+	var err error
+	var orderBatch []*match.AuctionOrder
+	for ; chanSize > 0; chanSize-- {
+		receivedOrder = <-auctionResultChannel
+		if receivedOrder.Err != nil {
+			logging.Errorf("Error came in with order solving result: %s", receivedOrder.Err)
+			// if there was an error, don't process the order
+			continue
+		}
+
+		if err = s.validateOrder(receivedOrder.Auction, receivedOrder.Encrypted); err != nil {
+			logging.Errorf("Error validating order: %s", err)
+			continue
+		}
+
+		logging.Infof("Order valid! Order placed by %x", receivedOrder.Auction.Pubkey)
+		orderBatch = append(orderBatch, receivedOrder.Auction)
+	}
+}
