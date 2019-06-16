c329fd6d98deb1d4f10bbcef6ef973952286dcfe
Rjected <kidscline01@gmail.com>
2019-06-15 21:04:52 -0400

    Added matching for auction server

--- match/engine.go
+++ match/engine.go
@@ -2,16 +2,28 @@ package match
 
 // The LimitEngine is the interface for the internal matching engine. This should be the lowest level
 // interface for the representation of a matching engine.
+// One of these should be made for every pair.
 type LimitEngine interface {
 	PlaceOrder(order *LimitOrder) (idRes *LimitOrderIDPair, err error)
 	CancelOrder(id *OrderID) (cancelled *CancelledOrder, err error)
-	MatchOrders(pair *Pair) (orderExecs []*OrderExecution, settlementExecs []*SettlementExecution, err error)
+	MatchOrders() (orderExecs []*OrderExecution, settlementExecs []*SettlementExecution, err error)
 }
 
 // The AuctionEngine is the interface for the internal matching engine. This should be the lowest level
 // interface for the representation of a matching engine.
+// One of these should be made for every pair.
 type AuctionEngine interface {
 	PlaceOrder(order *LimitOrder, auctionID *AuctionID) (idRes *AuctionOrderIDPair, err error)
 	CancelOrder(id *OrderID) (cancelled *CancelledOrder, err error)
-	MatchOrders(auctionID *AuctionID, pair *Pair) (orderExecs []*OrderExecution, settlementExecs []*SettlementExecution, err error)
+	MatchOrders(auctionID *AuctionID) (orderExecs []*OrderExecution, settlementExecs []*SettlementExecution, err error)
+}
+
+// SettlementEngine is an interface for something that keeps track of balances for users for a
+// certain asset.
+// One of these should be made for every asset.
+type SettlementEngine interface {
+	// ApplySettlementExecution is a method that applies a settlement execution.
+	ApplySettlementExecution(setExec *SettlementExecution) (err error)
+	// CheckValid is a method that returns true if the settlement execution would be valid.
+	CheckValid(setExec *SettlementExecution) (valid bool, err error)
 }
