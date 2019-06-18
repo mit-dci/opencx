55a80848c83439208c3e994271564216fbed5896
Rjected <kidscline01@gmail.com>
2019-06-18 16:51:02 -0400

    adding more abstraction for matching orders

--- match/limitorderidpair.go
+++ match/limitorderidpair.go
@@ -4,8 +4,8 @@ import "time"
 
 // LimitOrderIDPair is order ID, order, price, and time, used for generating executions in limit order matching algorithms
 type LimitOrderIDPair struct {
-	Timestamp time.Time
-	Price     float64
-	OrderID   *OrderID
-	Order     *LimitOrder
+	Timestamp time.Time   `json:"timestamp"`
+	Price     float64     `json:"price"`
+	OrderID   *OrderID    `json:"orderid"`
+	Order     *LimitOrder `json:"limitorder"`
 }
--- match/pricetime.go
+++ match/pricetime.go
@@ -29,40 +29,52 @@ func MatchPTPAlgorithm(book map[float64][]*LimitOrderIDPair) (orderExecs []*Orde
 // These are the orders that should match.
 func MatchPrioritizedOrders(buyOrders []*LimitOrderIDPair, sellOrders []*LimitOrderIDPair) (orderExecs []*OrderExecution, settlementExecs []*SettlementExecution, err error) {
 	// Lists should be in priority order starting at 0
-	var currBuyOrderExec *OrderExecution
-	var currSellOrderExec *OrderExecution
-	var currBuySetExec *SettlementExecution
-	var currSellSetExec *SettlementExecution
 	for len(buyOrders) > 0 && len(sellOrders) > 0 && buyOrders[0].Price <= sellOrders[0].Price {
-		currBuy := buyOrders[0]
-		currSell := sellOrders[0]
+		// Ahh whatever we can be a little inefficient space-wise, just add em all to the list
+		// and optimize later
 
-		// If the order ID is different then we're done with this order execution
-		if currBuyOrderExec.OrderID != *currBuy.OrderID {
-			orderExecs = append(orderExecs, currBuyOrderExec)
-			currBuyOrderExec = new(OrderExecution)
-		}
-		if currSellOrderExec.OrderID != *currSell.OrderID {
-			orderExecs = append(orderExecs, currSellOrderExec)
-			currSellOrderExec = new(OrderExecution)
-		}
+		// // If the order ID is different then we're done with this order execution
+		// if currBuyOrderExec.OrderID != *currBuy.OrderID {
+		// 	orderExecs = append(orderExecs, currBuyOrderExec)
+		// 	currBuyOrderExec = new(OrderExecution)
+		// }
+		// if currSellOrderExec.OrderID != *currSell.OrderID {
+		// 	orderExecs = append(orderExecs, currSellOrderExec)
+		// 	currSellOrderExec = new(OrderExecution)
+		// }
 
-		// If the order pubkey is different then we're done with this settlement execution
-		if currBuySetExec.Pubkey != currBuy.Order.Pubkey {
-			settlementExecs = append(settlementExecs, currBuySetExec)
-			currBuySetExec = new(SettlementExecution)
-		}
-		if currSellSetExec.Pubkey != currSell.Order.Pubkey {
-			settlementExecs = append(settlementExecs, currSellSetExec)
-			currSellSetExec = new(SettlementExecution)
-		}
+		// // If the order pubkey is different then we're done with this settlement execution
+		// if currBuySetExec.Pubkey != currBuy.Order.Pubkey {
+		// 	settlementExecs = append(settlementExecs, currBuySetExec)
+		// 	currBuySetExec = new(SettlementExecution)
+		// }
+		// if currSellSetExec.Pubkey != currSell.Order.Pubkey {
+		// 	settlementExecs = append(settlementExecs, currSellSetExec)
+		// 	currSellSetExec = new(SettlementExecution)
+		// }
 
 		// If sell was first, use that price
-		if currBuy.Timestamp.UnixNano() > currSell.Timestamp.UnixNano() {
-			// otherwise use the buy price
-		} else {
-
+		var prSellExec *OrderExecution
+		var prBuyExec *OrderExecution
+		var prelimSettlementExecs []*SettlementExecution
+		if prBuyExec, prSellExec, prelimSettlementExecs, err = MatchTwoOpposite(buyOrders[0], sellOrders[0]); err != nil {
+			err = fmt.Errorf("Error matching orders")
 		}
+
+		buyOrders[0].Order.AmountHave = prBuyExec.NewAmountHave
+		buyOrders[0].Order.AmountWant = prBuyExec.NewAmountWant
+
+		sellOrders[0].Order.AmountHave = prSellExec.NewAmountHave
+		sellOrders[0].Order.AmountWant = prSellExec.NewAmountWant
+
+		if prSellExec.Filled {
+			sellOrders = sellOrders[1:]
+		}
+		if prBuyExec.Filled {
+			buyOrders = buyOrders[1:]
+		}
+
+		settlementExecs = append(settlementExecs, prelimSettlementExecs...)
 
 	}
 	logging.Fatalf("UNIMPLEMENTED!!!")
@@ -74,5 +86,36 @@ func MatchPrioritizedOrders(buyOrders []*LimitOrderIDPair, sellOrders []*LimitOr
 // This makes it easy to put in to the MatchPrioritizedOrders algorithm.
 func PrioritizeOrderbookPTP(book map[float64][]*LimitOrderIDPair) (buyOrders []*LimitOrderIDPair, sellOrders []*LimitOrderIDPair, err error) {
 	logging.Fatalf("UNIMPLEMENTED!!!")
+	return
+}
+
+// MatchTwo matches a buy order order with the sell order supplied as an argument, giving this order priority.
+func MatchTwoOpposite(buyLp *LimitOrderIDPair, sellLp *LimitOrderIDPair) (buyExec *OrderExecution, sellExec *OrderExecution, settlementExecs []*SettlementExecution, err error) {
+
+	if buyLp.Order.Side != Buy || sellLp.Order.Side != Sell {
+		err = fmt.Errorf("Invalid input, buy LimitOrderIDPair was not buy or sell LimitOrderIDPair was not sell")
+		return
+	}
+	buyExec = &OrderExecution{
+		OrderID: *buyLp.OrderID,
+	}
+	sellExec = &OrderExecution{
+		OrderID: *sellLp.OrderID,
+	}
+
+	// So we have two LimitOrderIDPairs, and we're going to be generating executions for each
+	// as well as settlement executions, because we'll be checking price/time
+	if buyLp.Timestamp.UnixNano() > sellLp.Timestamp.UnixNano() {
+		// if the sell order amount have >= buy order amount want then we can just try to fill the buy
+		// order at the sell order's price
+		if sellLp.Order.AmountHave >= buyLp.Order.AmountHave {
+			return
+		}
+		// otherwise the sell order will be filled
+		return
+	}
+
+	// buy order is the priority one I guess
+
 	return
 }
