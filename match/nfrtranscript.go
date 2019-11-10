4ec3cf96966d95a4b82d4cb82de117f6aeb5a93d
Rjected <kidscline01@gmail.com>
2019-11-10 17:16:34 -0500

    finished trancript contents

--- match/nfrtranscript.go
+++ match/nfrtranscript.go
@@ -1,8 +1,16 @@
 package match
 
+// Transcript is the representation of a non front running proof
+// transcript. Puzzled orders are the "batch" and this should be able
+// to be verified quickly.
 type Transcript struct {
-	batchId    AuctionID `json:batchid`
-	batchIdSig []byte    `json:"signature"`
+	batchId          AuctionID                `json:batchid`
+	batchIdSig       []byte                   `json:"signature"`
+	puzzledOrders    []EncryptedSolutionOrder `json:"puzzledorders"`
+	commitment       [32]byte                 `json:"commitment"`
+	commitSig        []byte                   `json:"commitsig"`
+	commResponseSigs [][]byte                 `json:"commresponsesigs"`
+	decryptedOrders  []SolutionOrder          `json:"decryptedorders"`
 	// TODO: finish rest of transcript
 }
 
